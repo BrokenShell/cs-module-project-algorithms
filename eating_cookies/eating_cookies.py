5ea40bbfaa2aba7c1da9e255e4f214e2a2f0e9dc
BrokenShell <webmaster@sharpdesigndigital.com>
2020-07-28 07:48:23 -0700

    cookies update

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,7 @@ Returns: an integer
 
 def fib3(n):
     """ Fibonacci-3 Calculator """
-    a, b, c = 0, 1, 1
+    a, b, c = 1, 1, 2
     for _ in range(n):
         a, b, c = b, c, a + b + c
     return a
@@ -14,7 +14,7 @@ def fib3(n):
 
 def eating_cookies(n, *args):
     """ W(n) = W(n - 1) + W(n - 2) + W(n - 3) """
-    return fib3(n+1)
+    return fib3(n)
 
 
 if __name__ == "__main__":
