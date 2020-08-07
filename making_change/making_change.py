efb16cf08a95349de77229ee3b92deaa1fabe4a2
BrokenShell <webmaster@sharpdesigndigital.com>
2020-08-07 08:35:46 -0700

    making change update

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,16 +3,18 @@ import sys
 
 
 def making_change(amount, denominations):
-    # Your code here
-    pass
+    table = [0 for _ in range(amount + 1)]
+    table[0] = 1
+    for i in range(len(denominations)):
+        for j in range(denominations[i], amount + 1):
+            table[j] += table[j - denominations[i]]
+    return table[amount]
 
 
 if __name__ == "__main__":
-    # Test our your implementation from the command line
-    # with `python making_change.py [amount]` with different amounts
     if len(sys.argv) > 1:
-        denominations = [1, 5, 10, 25, 50]
-        amount = int(sys.argv[1])
-        print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+        d = [1, 5, 10, 25, 50]
+        a = int(sys.argv[1])
+        print(f"There are {making_change(a, d)} ways to make {a} cents.")
     else:
         print("Usage: making_change.py [amount]")
